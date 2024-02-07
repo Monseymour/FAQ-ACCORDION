b6cea4b22c5c1402c4b0165494b4659f6f98dd88
Monseymour <fabliniosales@gmail.com>
2024-02-07 18:32:55 -0300

    adicionado elemento (detail/summary)

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const botoes = Array.from(document.querySelectorAll("section"));
-const perguntas = Array.from(document.querySelectorAll(".pergunta"));
-const respostas = Array.from(document.querySelectorAll(".resposta"));
+const summaries = Array.from(document.querySelectorAll("summary"));
+const detail = document.querySelectorAll("details");
 const imagens = Array.from(document.querySelectorAll(".icones"));
 
-//  Ocultar 
-botoes.forEach((botao, indice) => {
-    botao.addEventListener("click", () => {
-        // Icones
+//  Trocar Icones
+summaries.forEach((summary, indice) => {
+    summary.addEventListener("click", () => {
         imagens.forEach(icones =>{
             icones.src = "./assets/images/icon-plus.svg"
         });
@@ -14,11 +12,16 @@ botoes.forEach((botao, indice) => {
         imagem.src = "./assets/images/icon-minus.svg";
     });
 });
-
-function mostrar(IdResposta){
-    const mostrando = document.getElementById(IdResposta);
-        respostas.forEach(resp =>{
-            resp.classList.add("off")
-        });
-        mostrando.classList.remove("off");
-};
+//  Ocultar Sumarios
+detail.forEach(d, () => {
+    d.addEventListener("click", () => { 
+        d.addEventListener('toggle', () => {
+            if (d.open) {
+              detail.forEach(otherDetails => {
+                if (otherDetails !== details) {
+                  otherDetails.open = false;
+                }}
+            )}
+        })
+    })
+});
